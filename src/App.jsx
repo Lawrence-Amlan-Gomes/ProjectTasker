8cb8af1a6c924ee31a58641b9f8730ddc703e9a1
Lawrence_Amlan_Gomes <amlangomes@gmail.com>
2024-01-17 19:12:47 +0600

    ProjectTasker Running 7.00 PM 17.1.24

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import HeroSection from "./Components/HeroSection";
+import TaskBoard from "./Components/TaskBoard";
+
 export default function App() {
-  return <div className="text-blue-800">Tasker</div>;
+  return (
+    <div>
+      <body className="bg-[#191D26] font-[Inter] text-white">
+        <Header />
+        <div className="flex flex-col items-center justify-center">
+          <HeroSection />
+          <TaskBoard />
+        </div>
+        <Footer />
+      </body>
+    </div>
+  );
 }
---
+++ src/Components/TaskBoard.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import SearchTask from "./SearchTask";
+import TaskActions from "./TaskActions";
+import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
+
+export default function TaskBoard(){
+    const defaultTask = {
+        id: crypto.randomUUID(),
+        title: "Learn React Native",
+        description:
+            "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
+        tags: ["web", "react", "js"],
+        priority: "High",
+        isFavorite: true,
+    };
+    // eslint-disable-next-line no-unused-vars
+    const [tasks, setTasks] = useState([defaultTask]);
+    const [showAddModal, setShowAddModal] = useState(false);
+    function handleAddTask() {
+        setShowAddModal(!showAddModal);
+    }
+
+    return (
+        <section className="mb-20" id="tasks">
+            {showAddModal && <AddTaskModal onAddClick={handleAddTask}/>}
+          <div className="container">
+
+            <SearchTask/>
+
+            <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+              
+              <TaskActions onAddClick={handleAddTask}/>
+
+              <TaskList tasks={tasks}/>
+
+            </div>
+          </div>
+        </section>
+    );
+}
